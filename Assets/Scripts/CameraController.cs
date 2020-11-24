51a8fe91e4bfa0df02a291614b93953ea0cbcf30
pytty <marttilamberg@hotmail.com>
2020-11-24 21:27:28 +0200

    2 axis rotating

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -39,10 +39,9 @@ public class CameraController : MonoBehaviour
         float speed = 10.0f;
 
         float tiltAroundY = Input.GetAxis("Mouse X") * speed;
-        //float tiltAroundX = Input.GetAxis("Mouse Y") * -speed;
+        float tiltAroundX = Input.GetAxis("Mouse Y") * -speed;
         // Do I have to use time.deltatime
-        transform.Rotate(0, tiltAroundY, 0);
-        //transform.Rotate(tiltAroundX, tiltAroundY, 0);
+        transform.localEulerAngles += new Vector3(tiltAroundX, tiltAroundY);
     }
     private void PanCamera()
     {
